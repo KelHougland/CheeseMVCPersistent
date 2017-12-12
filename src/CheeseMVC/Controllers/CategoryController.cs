e617a61fbeded5a00ad68386f2ba3cd675cd8cd3
Kelly Hougland <knhougland@gmail.com>
2017-12-12 10:28:59 -0600

    started over and committing early

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+
+    }
+}
